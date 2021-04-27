6e1b37e1455c7da5e11d70fd55bb45c0169569ac
Michał Lebiedzinski <lebiedzinski.michal@o2.pl>
2021-04-27 11:21:12 +0200

    Add project files.

---
+++ CityInformationsApp/CityInformationsApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CityInformationsApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CityInformationsApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
