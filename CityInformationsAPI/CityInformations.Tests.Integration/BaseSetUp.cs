9effac88e450f34e411198cd8b6b5c8ed304672c
Michał Lebiedziński <mlebiedzinski@gabos.pl>
2023-11-10 09:01:54 +0100

    Implement first Get test procedure for Events

---
+++ CityInformationsAPI/CityInformations.Tests.Integration/BaseSetUp.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CityInformations.Application.Helpers.MapperExtensions;
+using CityInformations.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+
+namespace CityInformations.Tests.Integration
+{
+    public class BaseSetUp : IDisposable
+    {
+        protected readonly MyDbContext DbContext;
+
+        public BaseSetUp()
+        {
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseInMemoryDatabase(databaseName: "InMemoryTestDatabase").Options;
+            DbContext = new MyDbContext(options);
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+        }
+
+        public IMapper GetMockMapper()
+        {
+            var mockMapper = Substitute.For<IMapper>();
+            var configurationProvider = new MapperConfiguration(cfg => {
+                cfg.AddProfile(new MapperProfile());
+            });
+            mockMapper.ConfigurationProvider.Returns(configurationProvider);
+
+            return mockMapper;
+        }
+    }
+}
---
+++ CityInformationsAPI/CityInformations.Tests.Integration/Features/Events/Get.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CityInformations.Application.Features.Events.Queries;
+using FluentAssertions;
+
+namespace CityInformations.Tests.Integration.Features.Events
+{
+    public class Get : BaseSetUp
+    {
+        [Fact]
+        public async Task When_Table_Is_Empty_Then_Return_Empty_List()
+        {
+            // arrange
+            IMapper mockMapper = GetMockMapper();
+            var handler = new GetAllItemsHandler(DbContext, mockMapper);
+
+            // act
+            var result = await handler.Handle(new GetAllItemsQuery(), CancellationToken.None);
+            var count = result.Count();
+            //assert
+            count.Should().Be(0);
+        }
+    }
+}
